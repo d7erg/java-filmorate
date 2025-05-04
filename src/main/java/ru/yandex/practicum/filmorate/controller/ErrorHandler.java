b1390ed1ec80fd576df614676903b3f114738805
d7erg <d7erg@yandex.com>
2025-05-04 21:07:16 +0300

    Refactor: улучшена обработка исключений с детальной обработкой каждого типа

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,22 +10,33 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.ErrorResponse;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleValidationException(MethodArgumentNotValidException e) {
+    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
+    public ErrorResponse handleValidationException(Exception e) {
+        log.error("Ошибка валидации", e);
+        if (e instanceof MethodArgumentNotValidException) {
+            return handleMethodArgumentNotValidException((MethodArgumentNotValidException) e);
+        } else if (e instanceof ConstraintViolationException) {
+            return handleConstraintViolationException((ConstraintViolationException) e);
+        }
         return new ErrorResponse(
                 "VALIDATION_ERROR",
-                e.getMessage(),
+                "Ошибка валидации",
                 HttpStatus.BAD_REQUEST.value()
         );
     }
 
-    @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
     public ErrorResponse handleNotFoundException(NotFoundException e) {
+        log.error("Ресурс не найден", e);
         return new ErrorResponse(
                 "NOT_FOUND",
                 e.getMessage(),
@@ -31,13 +44,43 @@ public class ErrorHandler {
         );
     }
 
-    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
     public ErrorResponse handleAllExceptions(Exception e) {
+        log.error("Внутренняя ошибка сервера", e);
         return new ErrorResponse(
                 "INTERNAL_ERROR",
-                "Произошла внутренняя ошибка сервера",
+                "Внутренняя ошибка сервера",
                 HttpStatus.INTERNAL_SERVER_ERROR.value()
+        );
+    }
+
+    private ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("Ошибка: MethodArgumentNotValidException", e);
+        List<String> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return new ErrorResponse(
+                "VALIDATION_ERROR",
+                String.join("; ", errors),
+                HttpStatus.BAD_REQUEST.value()
+        );
+    }
+
+    private ErrorResponse handleConstraintViolationException(ConstraintViolationException e) {
+        log.error("Ошибка: ConstraintViolationException", e);
+        List<String> errors = e.getConstraintViolations()
+                .stream()
+                .map(violation -> violation.getPropertyPath() + ": " + violation.getMessage())
+                .collect(Collectors.toList());
+
+        return new ErrorResponse(
+                "VALIDATION_ERROR",
+                String.join("; ", errors),
+                HttpStatus.BAD_REQUEST.value()
         );
     }
 }
